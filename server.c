36d1896fd310df012d7bdd6b41c2cea8d41cfc1a
DivSaumil <saumilkul@gmail.com>
2025-01-22 21:19:56 +0900

    now stores chat history

--- server.c
+++ server.c
@@ -52,9 +52,44 @@ void send_direct_message(const char *message, const char *recipient_username)
     pthread_mutex_unlock(&clients_mutex);
 }
 
-// Handle client messages
-void *client_handler(void *arg)
-{
+
+
+#define HISTORY_SIZE 1000 // Maximum number of messages in history
+char chat_history[HISTORY_SIZE][BUFFER_SIZE];
+int history_count = 0;
+
+// Store a message in the chat history
+void store_message_in_history(const char *message) {
+    pthread_mutex_lock(&clients_mutex);
+
+    if (history_count < HISTORY_SIZE) {
+        strncpy(chat_history[history_count], message, BUFFER_SIZE);
+        history_count++;
+    } else {
+        // Shift the history to make room for new messages
+        for (int i = 1; i < HISTORY_SIZE; i++) {
+            strncpy(chat_history[i - 1], chat_history[i], BUFFER_SIZE);
+        }
+        strncpy(chat_history[HISTORY_SIZE - 1], message, BUFFER_SIZE);
+    }
+
+    pthread_mutex_unlock(&clients_mutex);
+}
+
+// Send chat history to a client
+void send_chat_history(int client_socket) {
+    pthread_mutex_lock(&clients_mutex);
+
+    for (int i = 0; i < history_count; i++) {
+        send(client_socket, chat_history[i], strlen(chat_history[i]), 0);
+        send(client_socket, "\n", 1, 0);
+    }
+
+    pthread_mutex_unlock(&clients_mutex);
+}
+
+// Modify client_handler to handle /history
+void *client_handler(void *arg) {
     int client_socket = *(int *)arg;
     free(arg);
 
@@ -62,14 +97,12 @@ void *client_handler(void *arg)
     char username[50];
 
     // Receive username
-    if (recv(client_socket, username, sizeof(username), 0) <= 0)
-    {
+    if (recv(client_socket, username, sizeof(username), 0) <= 0) {
         close(client_socket);
         return NULL;
     }
-    username[strcspn(username, "\n")] = 0; // Remove newline character
+    username[strcspn(username, "\n")] = 0;
 
-    // Add client to the list
     pthread_mutex_lock(&clients_mutex);
     clients[client_count].socket = client_socket;
     strncpy(clients[client_count].username, username, sizeof(clients[client_count].username));
@@ -79,46 +112,37 @@ void *client_handler(void *arg)
     printf("%s connected.\n", username);
     sprintf(buffer, "%s has joined the chat.\n", username);
     broadcast_message(buffer, client_socket);
+    store_message_in_history(buffer);
 
-    while (1)
-    {
+    while (1) {
         int bytes_received = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
-        if (bytes_received <= 0)
-        {
+        if (bytes_received <= 0) {
             break;
         }
         buffer[bytes_received] = '\0';
 
-        // Check if the message is a direct message
-        if (buffer[0] == '@')
-        {
+        if (strcmp(buffer, "/history") == 0) {
+            send_chat_history(client_socket);
+        } else if (buffer[0] == '@') {
             char recipient[50];
             char message[BUFFER_SIZE];
-
-            // Parse the recipient username and message
             sscanf(buffer, "@%s %[^\n]", recipient, message);
 
             char formatted_message[BUFFER_SIZE];
             snprintf(formatted_message, sizeof(formatted_message), "[DM from %s]: %s\n", username, message);
             send_direct_message(formatted_message, recipient);
-        }
-        else
-        {
-            // Broadcast the message to all clients
+        } else {
             char formatted_message[BUFFER_SIZE];
             snprintf(formatted_message, sizeof(formatted_message), "[%s]: %s\n", username, buffer);
             broadcast_message(formatted_message, client_socket);
+            store_message_in_history(formatted_message);
         }
     }
 
-    // Remove client from the list
     pthread_mutex_lock(&clients_mutex);
-    for (int i = 0; i < client_count; i++)
-    {
-        if (clients[i].socket == client_socket)
-        {
-            for (int j = i; j < client_count - 1; j++)
-            {
+    for (int i = 0; i < client_count; i++) {
+        if (clients[i].socket == client_socket) {
+            for (int j = i; j < client_count - 1; j++) {
                 clients[j] = clients[j + 1];
             }
             client_count--;
@@ -129,11 +153,12 @@ void *client_handler(void *arg)
 
     sprintf(buffer, "%s has left the chat.\n", username);
     broadcast_message(buffer, client_socket);
-    printf("%s disconnected.\n", username);
+    store_message_in_history(buffer);
 
     close(client_socket);
     return NULL;
 }
+
 
 int main()
 {
